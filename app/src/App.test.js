30010c5a2b80d04eec27700418396d9db083ff3e
Fausto Gregori <faustogregori1985@gmail.com>
2020-01-15 01:51:45 +0000

    tests

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,8 +1,7 @@
-// counter.test.js
 import React from 'react'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import { render, fireEvent, getByTestId } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import reducer from './store/reducer'
 import initialState from './store/initialState'
@@ -14,9 +13,6 @@ function renderWithRedux(
 ) {
   return {
     ...render(<Provider store={store}>{ui}</Provider>),
-    // adding `store` to the returned utilities to allow us
-    // to reference it in our tests (just try to avoid using
-    // this to test implementation details).
     store,
   }
 }
